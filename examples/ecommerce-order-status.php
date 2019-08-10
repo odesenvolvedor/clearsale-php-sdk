f2901c09b89eeeb103af80e60f170cd8c8c47333
peter279k <peter279k@gmail.com>
2019-08-10 12:17:17 +0800

    Remove unused white spaces

--- examples/ecommerce-order-status.php
+++ examples/ecommerce-order-status.php
@@ -6,16 +6,16 @@ require_once __DIR__ . '/../autoload.php';
 
 try {
     $environment = new ClearSale\Environment\Environment(new ClearSale\Environment\Sandbox());
-    
+
     $auth = new \ClearSale\Auth\Login('user', 'password');
-    
+
     $orderRequest = new \ClearSale\Request\ClearSaleOrderRequest($environment, $auth);
-    
+
     $orderCode = 'ORDER_EXAMPLE_2_0_1';
-    
-    print_r($orderRequest->statusCheck($orderCode));    
-    
+
+    print_r($orderRequest->statusCheck($orderCode));
+
 } catch (ClearSale\Request\ClearSaleRequestException $exception) {
-    $error = $exception->getClearSaleError(); 
+    $error = $exception->getClearSaleError();
     print_r($exception);
 }
